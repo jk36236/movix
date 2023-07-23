c704b6be1fd9720fe3743d1d0f424b5d8c6e5a4a
jatin <jatinkalia08@gmail.com>
2023-07-23 17:23:14 +0530

    carousel section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner'
+import Trending from './trending/Trending'
 import './style.scss'
 
 
@@ -7,6 +8,7 @@ const home = () => {
   return (
     <div className="homePage">
       <HeroBanner/>
+      <Trending />
     </div>
   )
 }
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/switchtabs/SwitchTabs'
+
+
+const Trending = () => {
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+     <span className="carouselTitle">Trending</span>
+     <SwitchTabs />
+      </ContentWrapper>
+    </div>
+  )
+}
+
+export default Trending
