d5f5d005bf9cbf8b35809344e0664052760182b8
jatin <jatinkalia08@gmail.com>
2023-07-23 12:26:34 +0530

    header section

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    {/* <Header /> */}
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/:mediaType/:id" element={<Details />}/>
