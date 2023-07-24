e03816a3c4382c0db0b247df116cb3ac339dc4fc
jatin <jatinkalia08@gmail.com>
2023-07-24 10:56:35 +0530

    caarousel section -switching tabs

--- src/components/switchtabs/SwitchTabs.jsx
+++ src/components/switchtabs/SwitchTabs.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './style.scss'
 
-const SwitchTabs = () => {
+const SwitchTabs = ({data,onTabChange}) => {//destructure values which u passed as prop in switchtab omponenet in trrending component
+
+  //states
+  const [selectedTab,setSelectedTab]=useState(0)//isme hume index pass krna hai selected tab ka
+  const [left,setLeft]=useState(0);//for background animation
+
+const activeTab=(tab,index)=>{
+  setLeft(index * 100);//width of tab is 100,we have to mve it forward
+  setTimeout(() => {
+    setSelectedTab(index);//jis bhi tab  par click karenge uska index set kiya hai
+  }, 300);
+  onTabChange(tab,index);
+}
   return (
-    <div>SwitchTabs</div>
+    <div className="switchingTabs">
+      <div className="tabItems">
+        {/* //we will get the tabs from data by looping over it */}
+        {data.map((tab,index)=>(
+          //conditional class, tabitem by default rahegi and if selectedtab==index then add active class
+          <span key={index} className={`tabItem ${selectedTab ===index ? "active": ""}`}
+          onClick={()=>activeTab(tab,index)}>
+            {/* //iske andar jo humne day,week pas kra tha vo aa jayega */}
+            {tab}
+          </span>
+        ))}
+
+        <span className='movingBg' style={{left}}/>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchtabs/SwitchTabs'
-
+import useFetch from '../../../hooks/useFetch';
 
 const Trending = () => {
+  //state
+  const[endpoint,setEndPoint]=useState("day")//endpt initially will be day
+
+
+  //api call
+  //destructure data and loading from usefetch
+  // endpoint-/trending/{media_type}/{time_window}
+  ///trending/all(tv/movie)/endpoint(state)
+  const {data,loading}=useFetch(`/trending/all/${endpoint}`);
+
+  //in thuis method we have to call api when tab is changed
+  //and initially jab page load hoga toh day ka data sho krna hai and when e click on week then week ka data show karna hai
+  const onTabChange =(tab)=>{
+//is methid mein jis bhi tab par hum click karenge uska tabname mil jayega
+// agar tab day hai tab hume endpoint day set krna hai else week
+setEndPoint(tab==="Day"? "day":"week");
+  }
+
   return (
     <div className="carouselSection">
       <ContentWrapper>
      <span className="carouselTitle">Trending</span>
-     <SwitchTabs />
+     {/* pass an array of data and onchange method */}
+     <SwitchTabs data={["Day","Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   )
